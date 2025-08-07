cd726c2356b914f9ff2a3e36b4736af8e2368930
Sphis-Sinco <sinconsistencia@gmail.com>
2025-08-07 17:19:27 -0500

    idk

--- src/scenes/example.tsx
+++ src/scenes/example.tsx
@@ -9,4 +9,10 @@ export default makeScene2D(function* (view) {
   view.add(<Circle ref={circle} size={320} fill={'green'} />);
 
   yield* circle().scale(2, 2).to(1, 2);
+
+  yield* circle().fill('#e6a700', 1).to('#e13238', 1);
+  yield* circle().position.x(300, 1).to(-300, 1);
+  yield* circle().position.x(-300, 1).to(300, 1);
+  yield* circle().fill('#e13238', 1).to('#e6a700', 1);
+
 });
